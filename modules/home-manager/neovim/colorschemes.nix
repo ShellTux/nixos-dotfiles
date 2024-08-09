f9c7c23c62bf899da88424ff80188c2d58ecb892
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-08-09 16:32:59 +0100

    chore: modularise neovim

---
+++ modules/home-manager/neovim/colorschemes.nix
@@ -0,0 +1,13 @@
+{}:
+{
+	catppuccin.enable = true;
+	dracula.enable = true;
+	everforest.enable = true;
+	gruvbox.enable = true;
+	kanagawa.enable = true;
+	nightfox.enable = true;
+	nord.enable = true;
+	onedark.enable = true;
+	tokyonight.enable = true;
+	vscode.enable = true;
+}
---
+++ modules/home-manager/neovim/settings.nix
@@ -0,0 +1,52 @@
+{}:
+let
+	leader = " ";
+in
+{
+	globals = {
+		mapleader = leader;
+		maplocalleader = leader;
+		favorite_colorschemes = [
+			"adwaita"
+			"carbonfox"
+			"catppuccin"
+			"darkplus"
+			"duskfox"
+			"github_dark"
+			"gruvbox"
+			"kanagawa"
+			"kanagawa-dragon"
+			"nightfox"
+			"nordfox"
+			"onedark"
+			"terafox"
+			"tokyonight"
+			"vscode"
+		];
+	};
+
+	opts = {
+		background = "dark";
+		encoding = "UTF-8";
+		exrc = true;
+		hlsearch   = true;
+		ignorecase = true;
+		incsearch  = true;
+		linebreak  = false;
+		listchars = "tab:> ,trail:•,nbsp:~";
+		# mouse:append('a');
+		mouse = "nvia";
+		number         = true;
+		relativenumber = false;
+		scrolloff      = 999;
+		sidescrolloff  = 10;
+		signcolumn     = "yes";
+		smartcase      = true;
+		syntax         = "enabled";
+		termguicolors  = true;
+		textwidth      = 80;
+		wildignore     = "*.o,*.a,__pycache__";
+		wildmenu       = true;
+		wrap           = false;
+	};
+}
