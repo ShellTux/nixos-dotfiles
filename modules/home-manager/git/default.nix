a209b9a436e95bd046e4e9bd092da2d771b02d09
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-08-11 00:06:57 +0100

    fix(neovim): git aliases

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 {
 	options = {
 		git.enable = lib.mkEnableOption "Enable git module";
@@ -30,34 +30,35 @@
 				conflict = "diff --name-only --diff-filter=U";
 				cp = "cherry-pick";
 				diff-copy = ''!git diff-staged | ([ "$XDG_SESSION_TYPE" = "x11" ] && xclip -selection clipboard || wl-copy)'';
-				diff-last = ''!git diff HEAD~1 HEAD'';
+				diff-last = "!git diff HEAD~1 HEAD";
 				diff-staged = "diff --staged";
 				diff-summary = "diff --stat";
 				diff-word = "diff --word-diff --color-words";
 				forget = "update-index --assume-unchanged";
 				graph = "log --graph";
 				h = "history";
+				# FIX: history alias
 				history = "!~/.config/git/fshow";
-				ignore = ''!curl -sL https://www.toptal.com/developers/gitignore/api/$@'';
-				last = ''"!git log -1 HEAD"'';
+				ignore = "!curl -sL https://www.toptal.com/developers/gitignore/api/$@";
+				last = "!git log -1 HEAD";
 				lg1 = ''log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all'';
 				lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-				lg = ''"!git log --pretty=format:\"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\" --abbrev-commit -30"'';
+				lg = ''!git log --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]" --abbrev-commit -30'';
 				open = "visit";
 				patch = "!git --no-pager diff --no-color";
 				project-summary = "summary";
 				put = "push --set-upstream origin";
 				serve = "daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/";
 				showconfig = "config --list";
-				s = ''"status --short"'';
+				s = "status --short";
 				stats = "show --stat";
 				st = "status";
-				summary = "!which onefetch && onefetch";
+				summary = "!which ${pkgs.onefetch}/bin/onefetch && ${pkgs.onefetch}/bin/onefetch";
 				sw = "switch -";
-				touch = ''"!touch $@ && git add $@"'';
+				touch = "!touch $@ && git add $@";
 				unstage = "reset HEAD --";
-				visit-branch = ''"!xdg-open \"https://`git config --get remote.origin.url | sed -E \"s#(git@|git://|https?://|.git$)##g;s#:#/#\"`/tree/`git branch --show-current`\""'';
-				visit = ''"!xdg-open \"https://`git config --get remote.origin.url | sed -E \"s#(git@|git://|https?://|.git$)##g;s#:#/#\"`\""'';
+				visit-branch = ''!xdg-open "https://`git config --get remote.origin.url | sed -E "s#(git@|git://|https?://|.git$)##g;s#:#/#"`/tree/`git branch --show-current`"'';
+				visit = ''!xdg-open "https://`git config --get remote.origin.url | sed -E "s#(git@|git://|https?://|.git$)##g;s#:#/#"`"'';
 			};
 			delta = {
 				enable = false;
