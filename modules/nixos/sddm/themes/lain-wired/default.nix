3ee2d0e971315b32d15ff0390da6cb67400be73e
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-08-31 13:59:27 +0100

    feat(lain-wired): add gitlab repo

--- modules/nixos/sddm/themes/lain-wired/default.nix
+++ modules/nixos/sddm/themes/lain-wired/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchFromGitLab }:
 stdenv.mkDerivation rec {
 	name = "sddm-lain-wired-theme";
 	version = "0.9.1";
@@ -9,6 +9,13 @@ stdenv.mkDerivation rec {
 		rev = version;
 		sha256 = "0b0jqsxk9w2x7mmdnxipmd57lpj6sjj7il0cnhy0jza0vzssry4j";
 	};
+
+	# src = fetchFromGitLab {
+	# 	owner = "mixedCase";
+	# 	repo = name;
+	# 	rev = "4c98a329d62d40658657a2a4f54c44fe522c6117";
+	# 	sha256 = "0vaxd2ic5qmkwqivjdspbvkrr8ihp6rzq3hi42dryn4bn2w0nzps";
+	# };
 
 	installPhase = ''
 		mkdir -p $out/share/sddm/themes
