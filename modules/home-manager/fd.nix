675a8c49563f1af788ca90c10fb44a97cd0bc383
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-09-05 03:44:38 +0100

    feat(fd): add extraIgnores option to fd module

--- modules/home-manager/fd.nix
+++ modules/home-manager/fd.nix
@@ -1,12 +1,23 @@
-{ pkgs, lib, config, ... }:
+{ lib, config, ... }:
 {
-	options = {
-		fd.enable = lib.mkEnableOption "Enable fd module";
+	options.fd = {
+		enable = lib.mkEnableOption "Enable fd module";
+
+		extraIgnores = lib.mkOption {
+			type = lib.types.listOf lib.types.str;
+			default = [];
+			description = "List of extra paths that should be globally ignored.";
+		};
 	};
 
 	config = lib.mkIf config.fd.enable {
 		programs.fd = {
 			enable = true;
+
+			ignores = [
+				".git/"
+				"*.bak"
+			] ++ config.fd.extraIgnores;
 		};
 	};
 }
