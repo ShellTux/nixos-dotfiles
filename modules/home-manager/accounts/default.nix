156f3e89d0677d86ec9902f13ce1ff805b3d6c48
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-09-18 23:04:27 +0100

    add accounts.calendar

--- modules/home-manager/accounts/default.nix
+++ modules/home-manager/accounts/default.nix
@@ -3,6 +3,12 @@
 	options.accounts = {
 		enable = lib.mkEnableOption "Enable accounts module";
 
+		calendar.enable = lib.mkOption {
+			description = "Whether to enable calendar";
+			type = lib.types.bool;
+			default = true;
+		};
+
 		# FIX: failed assertion: profile must have exactly one primary mail when this option is false
 		email.enable = lib.mkOption {
 			description = "Whether to enable email";
@@ -12,6 +18,7 @@
 	};
 
 	imports = [
+		./calendar
 		./emails
 	];
 }
