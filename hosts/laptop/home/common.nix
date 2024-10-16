142a4d22ed4df63209382ebc2d0c9b726c336e20
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-10-16 02:56:19 +0000

    fix(home/common): auto-optimise-store is system config

--- hosts/laptop/home/common.nix
+++ hosts/laptop/home/common.nix
@@ -50,10 +50,7 @@
 	};
 
 	nix = {
-		settings = {
-			experimental-features = [ "nix-command" "flakes" ];
-			auto-optimise-store = true;
-		};
+		settings.experimental-features = [ "nix-command" "flakes" ];
 		gc = {
 			automatic = true;
 			options = "--delete-older-than 14d";
