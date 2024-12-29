c9373bce1dba2fd6891e4c255c53c9275e3d7f32
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-12-29 00:23:12 +0000

    feat(mktouch.sh): add verbose and help flags

--- pkgs/mktouch/mktouch.sh
+++ pkgs/mktouch/mktouch.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 set -e
 
+usage() {
+    printf 'Usage: %s <file> [...]\n' "$(basename "$0")"
+    echo
+    echo 'Flags:'
+    echo '   -h, --help'
+    echo '   -v, --verbose'
+    exit 1
+}
+
+verbose=false
+
+for arg
+do
+    if [ "$arg" = -v ] || [ "$arg" = --verbose ]
+    then
+        verbose=true
+    fi
+
+    if [ "$arg" = -h ] || [ "$arg" = --help ]
+    then
+        usage
+    fi
+done
+
 for arg
 do
 	file="$arg"
+        test -f "$file" || continue
+
 	mkdir --parents "$(dirname "$file")"
+        $verbose && echo touch "$file"
 	touch "$file"
 done
