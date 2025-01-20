68e945403807ff553f3194fc51590f3ce81cdbdc
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-01-20 18:47:26 +0000

    feat(nixvim): add noice recording macro status to lualine

--- modules/home-manager/apps/cli/neovim/nixvim/plugins/lualine.nix
+++ modules/home-manager/apps/cli/neovim/nixvim/plugins/lualine.nix
@@ -1,6 +1,7 @@
 { lib, config, ... }:
 let
   cfg = config.programs.nixvim.plugins.lualine;
+  noice-cfg = config.programs.nixvim.plugins.noice;
 in
 {
   programs.nixvim = {
@@ -10,9 +11,27 @@ in
     plugins.lualine = lib.mkIf cfg.enable {
       settings.sections = {
         lualine_a = [
-          {
+          (lib.mkIf noice-cfg.enable {
+            __unkeyed-1.__raw = ''
+              function()
+                return require('noice').api.statusline.mode.get():gsub('recording', '󰑊')
+              end
+            '';
+            cond.__raw = ''
+              function()
+                local noice = require("noice")
+                if noice.api.statusline.mode.has() and noice.api.statusline.mode.get():find("^recording") then
+                  return true
+                else
+                  return false
+                end
+              end
+            '';
+            color.fg = "#ff0000";
+          })
+          (lib.mkIf (noice-cfg.enable == false) {
             __raw = "function() local recording_register = vim.fn.reg_recording(); return (recording_register ~= '') and ('%#MacroRecording#󰑊 @' .. recording_register) or '' end";
-          }
+          })
           "mode"
         ];
         lualine_x = [
